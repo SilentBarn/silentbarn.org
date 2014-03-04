2dac750fa54766613a606352a41dd6469196bb2a
Mike Gioia <mikegioia@gmail.com>
2014-03-03 23:14:41 -0500

    Fixed sql scripts

--- deploy/sql/013_event_times.sql
+++ deploy/sql/013_event_times.sql
@@ -1,5 +1,5 @@
 INSERT INTO  `silentbarn`.`inserts` ( `name` , `created_at` )
 VALUES ( '013_event_times', NOW( ) );
 
-ALTER TABLE  `posts` ADD  `event_time` TIME NULL DEFAULT NULL AFTER  `event_date_end` ,
+ALTER TABLE  `silentbarn`.`posts` ADD  `event_time` TIME NULL DEFAULT NULL AFTER  `event_date_end` ,
 ADD  `event_time_end` TIME NULL DEFAULT NULL AFTER  `event_time` ;
