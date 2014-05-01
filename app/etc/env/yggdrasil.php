db3c5fe1a81ddda58396b46ee6313cc770bd7fe0
Mike Gioia <mikegioia@gmail.com>
2014-05-01 15:35:53 -0400

    changed build env to dev for now, prod mode not enabled

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'jsEnvironment' => 'production' ],
+        'jsEnvironment' => 'development' ],
 
     'cache' => [
         'adapter' => 'redis' ],
