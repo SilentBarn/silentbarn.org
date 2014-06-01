10aefe333b63437e21a5dc308f97eac622fc6754
Mike Gioia <mikegioia@gmail.com>
2014-06-01 00:21:14 -0400

    more css updates, rebuilt and incremented version

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'assetVersion' => 7,
+        'assetVersion' => 8,
         'assetMode' => 'production' ],
 
     'cache' => [
