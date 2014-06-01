53c56c462086c6e5d1e6e4a039c96cdabe8096ef
Mike Gioia <mikegioia@gmail.com>
2014-06-01 16:36:12 -0400

    fixed footer mq styles. rebuilt and reincremented.

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'assetVersion' => 12,
+        'assetVersion' => 13,
         'assetMode' => 'production' ],
 
     'cache' => [
