02b66d3e5a9c44e4f98c0c2a2b04551d449eaab3
Mike Gioia <mikegioia@gmail.com>
2014-06-02 14:34:02 -0400

    updated mobile styles for all pages except the stewdios pages, gallery, and chefs.

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'assetVersion' => 20,
+        'assetVersion' => 21,
         'assetMode' => 'production' ],
 
     'cache' => [
