143cdb3a8d425eb4deb66bbab007041c068d8ac8
Mike Gioia <mikegioia@gmail.com>
2014-09-23 08:57:46 -0400

    Added support for vimeo, youtube and soundcloud links

--- app/models/Sql/Posts.php
+++ app/models/Sql/Posts.php
@@ -383,7 +383,47 @@ class Posts extends \Base\Model
      */
     function getHtmlBody()
     {
-        return Markdown::defaultTransform( nl2br( $this->body ) );
+        // get html from markdown
+        $html = Markdown::defaultTransform( $this->body );
+
+        // process any youtube videos
+        $youtubeEmbed = sprintf(
+            '<iframe width="%s" height="%s" src="%s/%s?rel=0" '.
+                'frameborder="0" allowfullscreen></iframe>',
+            '670',
+            '380',
+            '//www.youtube.com/embed',
+            '$1' ); // preg variable of video ID
+        $html = preg_replace( "/\[\#youtube:(.*?)\]/", $youtubeEmbed, $html );
+
+        // process any vimeo videos
+        $vimeoEmbed = sprintf(
+            '<iframe src="%s/%s?%s" width="%s" height="%s"' .
+                'frameborder="0" allowfullscreen></iframe>',
+            '//player.vimeo.com/video',
+            '$1', // preg variable of video ID
+            'title=0&portrait=0&badge=0',
+            '670',
+            '380' );
+        $html = preg_replace( "/\[\#vimeo:(.*?)\]/", $vimeoEmbed, $html );
+
+        // process any soundcloud links
+        $soundcloudEmbed = sprintf(
+            '<iframe width="%s" height="%s" scrolling="no" frameborder="no" '.
+                'src="%s/%s%s"></iframe>',
+            '100%',
+            '166',
+            'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks',
+            '$1', // preg variable of cloud ID
+            '&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true' );
+        $html = preg_replace( "/\[\#soundcloud:(.*?)\]/", $soundcloudEmbed, $html );
+
+        // process any images
+        $img = '<img src="$1" alt="" title="" />';
+        $html = preg_replace( "/\[\#image:(.*?)\]/", $img, $html );
+
+        return $html;
+        //return Markdown::defaultTransform( nl2br( $this->body ) );
     }
 
     /**
