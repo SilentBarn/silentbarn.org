1fd432797bebedc737b15b6687de52f8de1ccfa2
Mike Gioia <mikegioia@gmail.com>
2014-12-21 14:40:32 -0500

    Incremented asset version

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'assetVersion' => 30,
+        'assetVersion' => 31,
         'assetMode' => 'production',
         'analytics' => [
             'enabled' => TRUE,
