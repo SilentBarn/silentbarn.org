f64f382cf0d3c186717fa1777f0af6298191b4f0
Mike Gioia <mikegioia@gmail.com>
2015-03-13 15:59:29 -0700

    Wow, really fixing sql statement

--- deploy/sql/025_publish_setting.sql
+++ deploy/sql/025_publish_setting.sql
@@ -1,4 +1,4 @@
 INSERT INTO  `silentbarn`.`inserts` ( `name` , `created_at` )
 VALUES ( '025_publish_setting', NOW( ) );
 
-ALTER TABLE  `silentbarn`.`users` ADD  `silentbarn`.`access_publish` TINYINT( 1 ) UNSIGNED NULL DEFAULT  '0' AFTER  `access_homepage` ;
+ALTER TABLE  `silentbarn`.`users` ADD  `users`.`access_publish` TINYINT( 1 ) UNSIGNED NULL DEFAULT  '0' AFTER  `access_homepage` ;
