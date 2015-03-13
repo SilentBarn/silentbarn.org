0851d43f3bc2243bf8d5a0cc31c86a2b90ed1441
Mike Gioia <mikegioia@gmail.com>
2015-03-13 15:57:23 -0700

    Fixed database name in script

--- deploy/sql/025_publish_setting.sql
+++ deploy/sql/025_publish_setting.sql
@@ -1,4 +1,4 @@
 INSERT INTO  `silentbarn`.`inserts` ( `name` , `created_at` )
 VALUES ( '025_publish_setting', NOW( ) );
 
-ALTER TABLE  `users` ADD  `silentbarn`.`access_publish` TINYINT( 1 ) UNSIGNED NULL DEFAULT  '0' AFTER  `access_homepage` ;
+ALTER TABLE  `silentbarn`.`users` ADD  `silentbarn`.`access_publish` TINYINT( 1 ) UNSIGNED NULL DEFAULT  '0' AFTER  `access_homepage` ;
