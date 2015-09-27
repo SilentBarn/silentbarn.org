f7aa54642afaefdbe1c9876fe20769c9885ae3cf
Mike Gioia <mikegioia@gmail.com>
2015-09-27 10:55:05 -0400

    Added header for the fire update and a link to donate. Recompiled assets.

--- app/etc/env/yggdrasil.php
+++ app/etc/env/yggdrasil.php
@@ -6,7 +6,7 @@ return [
         'responseMode' => 'view',
         'modules' => [
             'admin' => 'Admin' ],
-        'assetVersion' => 38,
+        'assetVersion' => 39,
         'assetMode' => 'production',
         'analytics' => [
             'enabled' => TRUE,
