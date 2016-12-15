5244454d65c5b3e26adbfad5f66e44faafde0b71
Mike Gioia <mikegioia@gmail.com>
2016-12-15 15:25:39 -0500

    Added sanctum config

---
+++ app/etc/env/sanctum.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'app' => [
+        'environment' => 'sanctum',
+        'responseMode' => 'view',
+        'modules' => [
+            'admin' => 'Admin' ],
+        'assetVersion' => 43,
+        'assetMode' => 'production',
+        'analytics' => [
+            'enabled' => TRUE,
+            'code' => 'UA-37484438-1'
+        ]],
+    'cache' => [
+        'adapter' => 'redis' ],
+
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '##SQLPASSWORD##',
+        'dbname' => 'silentbarn',
+        'persistent' => TRUE ],
+
+    'paths' => [
+        'baseUri' => 'http://sanctum.silentbarn.org/',
+        'assetUri' => 'http://sanctum.silentbarn.org/assets/%s/',
+        'hostname' => 'sanctum.silentbarn.org',
+        'media' => '/var/www/sanctum.silentbarn.org/www-data/media',
+        'mediaPublic' => 'http://sanctum.silentbarn.org/media/' ],
+
+    'cookies' => [
+        'secure' => FALSE ],
+
+    'profiling' => [
+        'query' => FALSE ],
+
+    'instagram' => [
+        'clientId' => '##INSTAGRAMCLIENTID##' ],
+
+    'mailgun' => [
+        'to' => [
+            'rentals' => '##MAILGUNTORENTALS##',
+            'events' => '##MAILGUNTOEVENTS##',
+            'stewdios' => '##MAILGUNTOSTEWDIOS##' ],
+        'smtp' => [
+            'hostname' => '##MAILGUNHOSTNAME##',
+            'username' => '##MAILGUNUSERNAME##',
+            'password' => '##MAILGUNPASSWORD##' ]
+    ]];
