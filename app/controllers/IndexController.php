eca6795f230f570a525257e578ed4048aac44015
Mike Gioia <mikegioia@gmail.com>
2018-03-10 08:02:35 -0800

    Updated to query param

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -11,11 +11,13 @@ class IndexController extends \Base\Controller
         return parent::beforeExecuteRoute();
     }
 
-    public function indexAction( $hideOverlay = "" )
+    public function indexAction()
     {
+        $hideOverlay = $this->request->getQuery( 'hide' ) == 1;
+
         $this->view->pick( 'home/index' );
         // In demo mode, when ready for prod remove "!"
-        $this->view->hideOverlay = ! strlen( trim( $hideOverlay ) );
+        $this->view->hideOverlay = ! $hideOverlay;
         $this->view->boxPosts = \Db\Sql\Posts::getByLocation( 'boxes', 5 );
         $this->view->heroPosts = \Db\Sql\Posts::getByLocation( 'hero', 10 );
     }
