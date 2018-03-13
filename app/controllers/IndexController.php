2287515e40e9f403754393a914423f3cf199410d
Mike Gioia <mikegioia@gmail.com>
2018-03-12 18:50:46 -0700

    Content updates for final announcement

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -13,12 +13,15 @@ class IndexController extends \Base\Controller
 
     public function indexAction()
     {
-        $hideOverlay = $this->request->getQuery( 'hide' ) == 1;
-
-        $this->view->pick( 'home/index' );
-        // In demo mode, when ready for prod remove "!"
-        $this->view->hideOverlay = ! $hideOverlay;
         $this->view->boxPosts = \Db\Sql\Posts::getByLocation( 'boxes', 5 );
         $this->view->heroPosts = \Db\Sql\Posts::getByLocation( 'hero', 10 );
+
+        // Reverse this in prod mode
+        if ( $this->request->getQuery( 'hide' ) == 1 ) {
+            $this->view->pick( 'home/final' );
+        }
+        else {
+            $this->view->pick( 'home/index' );
+        }
     }
 }
